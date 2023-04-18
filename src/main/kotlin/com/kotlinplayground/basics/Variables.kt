2c451cd9d43a3d972a738bb3cafa9798c06fe6cb
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-18 12:38:26 +0600

    String interpolation or string template techniques

--- src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -9,10 +9,18 @@ fun main() {
     var age = 34
     println(age)
     age = 35
-    age = 40
     println(age)
 
     val course = "Kotlin sprint"
     // String interpolation or string template techniques
     println("Course: $course and the course length is ${course.length}")
+
+    val multiline = "ABC \n DEF" // multiline (general syntax)
+    println(multiline)
+    // triple quotes multiline (general syntax)
+    val multiline1 = """
+        ABD
+        DEF
+    """.trimIndent()
+    println(multiline1)
 }
