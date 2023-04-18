6bae0f8b3a4946d4d3c7b931615e1c87a406003b
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-18 16:37:15 +0600

    Default value parameters and Named argument/parameter

--- src/main/kotlin/com/kotlinplayground/functions/functions.kt
+++ src/main/kotlin/com/kotlinplayground/functions/functions.kt
@@ -1,5 +1,7 @@
 package com.kotlinplayground.functions
 
+import java.time.LocalDate
+
 /**
  * The type with only one value: the Unit object. This type corresponds to the void type in Java.
  */
@@ -18,11 +20,27 @@ fun addition(x: Int, y: Int): Int {
 //fun additionApproach1(x: Int, y: Int): Int = x + y
 fun additionApproach1(x: Int, y: Int) = x + y
 
+fun printPersonDetails(
+    name: String,
+    email: String = "",
+    dob: LocalDate = LocalDate.now()
+) {
+    println("My name is $name and my email is '$email' and dob is $dob")
+}
+
 fun main() {
-    printName("Ikram")
-    val result = addition(1, 2)
-    println("result is $result")
-    val result1 = additionApproach1(1, 2)
-    println("result1 is $result1")
+//    printName("Ikram")
+//    val result = addition(1, 2)
+//    println("result is $result")
+//    val result1 = additionApproach1(1, 2)
+//    println("result1 is $result1")
+
+    // Default value parameters
+//    printPersonDetails("Ikram", "abc@gmail.com", LocalDate.parse("2003-01-01"))
+//    printPersonDetails("Ikram")
+
+    // Named argument or Named para
+//    printPersonDetails(name = "Ikram")
+    printPersonDetails(name = "Ikram", dob = LocalDate.parse("2003-01-05"))
 }
 
