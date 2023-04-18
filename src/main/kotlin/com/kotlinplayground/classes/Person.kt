6376e243ceb1547ee82b170f2122414eb5a23db6
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-18 18:21:22 +0600

    primary construction

--- src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -1,12 +1,21 @@
 package com.kotlinplayground.classes
 
-class Person {
+class Person(
+    val name: String = "",
+    val age: Int = 0
+) {
   fun action() {
-    println("Person walks")
+    println("$name, age of $age, He/she walks")
   }
 }
 
 fun main() {
-  val person = Person() // No need use 'new' keyword for instantiate a class
+  val person = Person("Ikram", 34) // No need use 'new' keyword for instantiate a class
     person.action()
+    println("Person name is ${person.name}")
+    println("Person age is ${person.age}")
+
+    val person1 = Person()
+    println("Person name is ${person1.name}")
+    println("Person age is ${person1.age}")
 }
