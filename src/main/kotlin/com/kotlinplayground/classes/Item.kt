4912ce7fe26c66dec30d452e5e92e2f6adeff335
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-19 12:14:33 +0600

    custom getter and setter

--- src/main/kotlin/com/kotlinplayground/classes/Item.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Item.kt
@@ -1,7 +1,23 @@
 package com.kotlinplayground.classes
 
+import java.lang.IllegalArgumentException
+
 class Item {
   var name: String = ""
+  var price: Double = 0.0
+    get() {
+      println("Inside Getter")
+      return field
+    }
+    set(value) {
+      if(value>0.0) {
+        println("Inside Setter")
+        field = value
+      } else {
+        throw IllegalArgumentException("Negative value doesn't allowed")
+      }
+    }
+
   constructor(itemName: String) {
     name = itemName
   }
@@ -11,5 +27,8 @@ fun main() {
   val item = Item("iPhone")
     println("Item name is ${item.name}")
     println("Item name is $item")
+
+    item.price = 10.0
+    println("${item.price}")
 
 }
