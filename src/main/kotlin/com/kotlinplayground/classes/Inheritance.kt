e7bb15595992ad4d25553501c71c61e4d0d87c61
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-19 19:13:27 +0600

    Visibility modifiers

--- src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
@@ -5,6 +5,14 @@ open class User(val name: String) {
     open fun login() {
         println("Inside User class")
     }
+
+    private fun secret() {
+        println("Inside User class")
+    }
+
+    protected open fun logout() {
+        println("Inside User class")
+    }
 }
 
 class Student(name: String) : User(name) {
@@ -16,8 +24,13 @@ class Student(name: String) : User(name) {
         fun country() = "Bangladesh"
     }
     override fun login() {
-        println("Inside Student class")
+        println("Inside Student login")
         super.login()
+    }
+
+    public override fun logout() {
+        println("Inside Student logout")
+        super.logout()
     }
 }
 
@@ -26,6 +39,7 @@ class Instructor(name: String) : User(name)
 fun main() {
     val student = Student("Ikram")
     student.login()
+    student.logout()
     student.isLoggedIn = true
     println("Student name is ${student.name}")
     val country = Student.country()
@@ -36,4 +50,7 @@ fun main() {
     val instructor = Instructor("John")
     instructor.login()
     println("Instructor name is ${instructor.name}")
+
+    val user = User("Ikram")
+//    user.
 }
