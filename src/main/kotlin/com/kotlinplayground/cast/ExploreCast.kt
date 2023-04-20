59891cab208ec57d0921f77ff481fa6aa0a60145
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-20 12:24:43 +0600

    Type checking and casting and smart cast

---
+++ src/main/kotlin/com/kotlinplayground/cast/ExploreCast.kt
@@ -0,0 +1,33 @@
+package com.kotlinplayground.cast
+
+import com.kotlinplayground.classes.Course
+
+fun main() {
+    val course = Course(1, "Reactive programming in Modern Java using project reactor", "Ikram")
+
+    checkType(course)
+    checkType("course")
+    castNumber(1.0)
+//    castNumber(1)
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+}
+
+fun castNumber(any: Any) {
+    when(any) {
+        any as Double -> println("Value is Double")
+    }
+}
+
+fun checkType(type: Any) {
+    when(type) {
+        is Course -> {
+            println(type)
+        }
+        is String -> {
+            println("Yes, i'm the string")
+        }
+    }
+}
