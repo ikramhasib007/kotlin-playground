e7e37452793a156b53293fcbafb8fe080dd0a286
Ikram Ud Daula (Contractor) <ikram.daula@greenlight.me>
2023-04-20 16:08:20 +0600

    invoking or assigning a nullable type to a non-nullable type

--- src/main/kotlin/com/kotlinplayground/nulls/ExploreNulls.kt
+++ src/main/kotlin/com/kotlinplayground/nulls/ExploreNulls.kt
@@ -4,9 +4,14 @@ data class Movie (
     val id: Int?,
     val name: String
 )
+
 fun main() {
     var nameNullable : String? = null
     println("Here some data: $nameNullable")
+//    printName(nameNullable!!)
+    nameNullable?.run {             // run is a scope function
+        printName(this)
+    }
     val length = nameNullable?.length ?: 0 // Safe operator ? // Elvis operator ?:
     println("Safe operator [length]: $length")
     nameNullable = "Ikram"
@@ -14,13 +19,21 @@ fun main() {
     println("Safe operator : ${nameNullable?.length}")
 
     var name: String = "Hasib"
-    println(name)
+    printName1(name)
 
     val movie = Movie(null, "Avengers")
     val savedMovie = saveMovie(movie)
     savedMovie.id!!                         // non null assertion !!
     println(savedMovie.id)
     println("Saved movie: $savedMovie")
+}
+
+fun printName(name: String) {
+    println(name)
+}
+
+fun printName1(name: String?) {
+    println("Non name: $name")
 }
 
 fun saveMovie(movie: Movie): Movie {
