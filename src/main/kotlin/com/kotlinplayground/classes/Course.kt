a1bf478ed880697fe94524fb0d81d3da17ff7b6e
ikramhasib007 <ikramhasib007@gmail.com>
2023-04-20 12:47:12 +0600

    enum

--- src/main/kotlin/com/kotlinplayground/classes/Course.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Course.kt
@@ -4,12 +4,20 @@ data class Course(
     val id: Int,
     val name: String,
     val author: String,
+    val courseCategory: CourseCategory = CourseCategory.DEVELOPMENT
 )
+
+enum class CourseCategory {
+    DEVELOPMENT,
+    BUSINESS,
+    DESIGN,
+    MARKETING
+}
 
 fun main() {
     val course = Course(1, "Reactive programming in Modern Java using project reactor", "Ikram")
     val course1 = Course(2, "Reactive programming in Modern Java using project reactor", "Ikram")
-//    println("Print data class: $course")
+    println("Print data class: $course")
     println("Checking object equality: ${course == course1}")
 
     // Clone the object by using copy method
@@ -17,4 +25,7 @@ fun main() {
         id = 3, author = "Ikram1"
     )
     println(course3)
+
+    val marketingCourse = Course(1, "Reactive programming in Modern Java using project reactor", "Ikram", CourseCategory.MARKETING)
+    println(marketingCourse)
 }
