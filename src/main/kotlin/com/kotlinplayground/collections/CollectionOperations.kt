bca3cf27a97ef69bff3cc8194b6196b5d91ad392
Ikram Ud Daula (Contractor) <ikram.daula@greenlight.me>
2023-04-28 17:58:28 +0600

    Map operations on kotlin collections

--- src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
+++ src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
@@ -10,13 +10,35 @@ fun main() {
     val devPredicate = { c:Course -> c.category == CourseCategory.DEVELOPMENT }
     val desPredicate = { c:Course -> c.category == CourseCategory.DESIGN }
 
-    exploreFilter(courseList, desPredicate)
+    // exploreFilter(courseList, desPredicate)
+
+    // exploreMap(courseList) only map
+    exploreMap(courseList, desPredicate) // combine with filter and map
+}
+
+//fun exploreMap(courseList: MutableList<Course>) {
+//    val courses = courseList
+//        .map { it.name } // will return new list
+//        .forEach { // forEach doesn't return any item, that's mean it will return Unit
+//            println(it)
+//        }
+//    println("courses: $courses")
+//}
+
+fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val courses = courseList
+        .filter(predicate) // Note: filter() invoke predicate automatically
+        .map { it.name } // will return new list
+        .forEach { // forEach doesn't return any item, that's mean it will return Unit
+            println(it)
+        }
+    println("courses: $courses")
 }
 
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
     courseList
 //        .filter { it.category == CourseCategory.DEVELOPMENT }
-        .filter { predicate.invoke(it) }
+        .filter { predicate.invoke(it) } // Note: filter {} invoke predicate manually
         .forEach {
             it
             println("devCourse: $it")
