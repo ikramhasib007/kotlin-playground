cf8dbddcf754b0a13b3e158e1a7f2f60a8dbe018
ikramhasib007 <ikramhasib007@gmail.com>
2023-05-04 16:52:13 +0600

    setting up different profiles in spring boot

--- src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
@@ -1,9 +1,14 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService {
 
-    fun retrieveGreeting(name: String) = "Hello $name"
+    @Value("\${message}") // @Value, which read the profile (dynamically loads when application starts, application.yml file under the resources' folder) and assign to the variable
+    lateinit var message: String // lateinit modifier does the init the variable after the spring boot application runs
+
+
+    fun retrieveGreeting(name: String) = "$name, $message"
 }
