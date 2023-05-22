d8b3a7650f442db170a3291c0ba9c58e63399aba
Ikram Ud Daula (Contractor) <ikram.daula@greenlight.me>
2023-05-22 20:27:47 +0600

    Retrieve Courses By Name using JPA Query Creation Function

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -4,4 +4,7 @@ import com.kotlinspring.entity.Course
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository: CrudRepository<Course, Int> { // Interaction with Course entity and Int is auto generated id for the entity
+
+    // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+    fun findByNameContaining(courseName: String) : List<Course> // creating custom query
 }
---
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
@@ -0,0 +1,30 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+
+@DataJpaTest // For slice of the application/spring context, that is make that DB layer just available to test
+@ActiveProfiles("test") // For make sure we are going to use the test context only
+class CourseRepositoryIntegrationTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+
+        courseRepository.deleteAll()
+        courseRepository.saveAll(courseEntityList())
+    }
+    @Test
+    fun findByNameContaining() {
+        val courses = courseRepository.findByNameContaining("SpringBoot")
+        println("courses: $courses")
+        assertEquals(2, courses.size)
+    }
+}
