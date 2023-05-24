df5ab72f244654113a0b1dbdad109aecd41165f6
Ikram Ud Daula (Contractor) <ikram.daula@greenlight.me>
2023-05-24 13:25:49 +0600

    Testing multiple sets of data using Parameterized test

--- src/test/intg/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
@@ -4,9 +4,13 @@ import com.kotlinspring.util.courseEntityList
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest // For slice of the application/spring context, that is make that DB layer just available to test
 @ActiveProfiles("test") // For make sure we are going to use the test context only
@@ -34,5 +38,20 @@ class CourseRepositoryIntegrationTest {
         val courses = courseRepository.findCourseByName("SpringBoot")
         println("courses: $courses")
         assertEquals(2, courses.size)
+    }
+
+    @ParameterizedTest // You can run multiple test in a single test by defining @MethodSource annotation
+    @MethodSource("courseAndSize") // We can extract the input data by passing a function
+    fun findCourseByNameApproach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCourseByName(name)
+        println("courses: $courses")
+        assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+        @JvmStatic // Need to tell java, this is a static function
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("SpringBoot", 2), Arguments.arguments("Wiremock", 1))
+        }
     }
 }
